776c23a625da4124ad0cf1fac0bfad6aeff50f95
electromeow <gokgoz.berat123@gmail.com>
2021-04-28 05:40:34 +0300

    License notices added.

--- database.py
+++ database.py
@@ -1,3 +1,22 @@
+"""
+Copyright (C) 2021  Berat Gökgöz
+
+This file is a part of Timetable project.
+
+Timetable is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any
+later version.
+
+Timetable is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import psycopg2 as sql
 import pandas as pd
 import json
